3ff37514c74dfc6409b3afe4e3ff1f1d979706a8
Hudson Rodrigues <hudson_rs@outlook.com>
2018-09-01 02:27:41 -0300

    Detalhes do Produto

--- site.php
+++ site.php
@@ -42,4 +42,18 @@
 			'pages'=>$pages
 		]);
 	});
+
+	$app->get("/products/:desurl", function($desurl) {
+
+		$product = new Product();
+
+		$product->getFromURL($desurl);
+
+		$page = new Page();
+
+		$page->setTpl("product-detail", [
+			'product'=>$product->getValues(),
+			'categories'=>$product->getCategories()
+		]);
+	});
 ?>
