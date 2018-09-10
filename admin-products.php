2bbf523f275932eb3774be13dfaa22ce03e1e9ad
Hudson Rodrigues <hudson_rs@outlook.com>
2018-09-10 11:23:14 -0300

    Admin - Produtos - Busca e Paginacao

--- admin-products.php
+++ admin-products.php
@@ -9,12 +9,40 @@
 
 		User::verifyLogin();
 
-		$products = Product::listAll();
+		$search = (isset($_GET['search'])) ? $_GET['search'] : "";
+
+		$pag = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+
+		if($search != ''){
+
+			$pagination = Product::getPageSearch($search,$pag);
+
+		} else {
+
+			$pagination = Product::getPage($pag);
+
+		}
+
+
+		$pages = [];
+
+		for($x = 0; $x < $pagination['pages']; $x++){
+
+			array_push($pages, [
+				'href'=>'/admin/products?'.http_build_query([
+					'page'=>$x+1,
+					'search'=>$search
+				]),
+				'text'=>$x+1
+			]);
+		}
 
 		$page = new PageAdmin();
 
 		$page->setTpl("products", [
-			"products"=>$products
+			"products"=>$pagination['data'],
+			"search"=>$search,
+			"pages"=>$pages
 		]);
 
 	});
